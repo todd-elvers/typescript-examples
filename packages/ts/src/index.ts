88a70c9da9089c357d7b951206dd4b16f708d749
Todd Elvers <toddelvers@carfax.com>
2019-07-14 22:12:27 -0500

    Initial commit

---
+++ packages/ts/src/index.ts
@@ -0,0 +1,42 @@
+console.log("Welcome to TypeScript.");
+
+//
+// Creating an object, implementing it, and using it with a method
+//
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+const bill: Person = {
+  firstName: "Bill",
+  lastName: "Smith"
+};
+
+const personGreeter = (person: Person): string =>
+  `Hello ${person.firstName} ${person.lastName}!`;
+
+console.log(personGreeter(bill));
+
+//
+// Extending the above example with OOP practices
+//
+class Student implements Person {
+  fullName: string;
+
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+const studentGreeter = (student: Student): string =>
+  `Hello ${student.fullName}!`;
+
+let user = new Student("Jane", "M.", "User");
+
+console.log(personGreeter(user)); // Student objects implement Person, so this works
+console.log(studentGreeter(user));
